d9fac61cabcc587751deff1d62310500bafcc7d8
Badgrif <aswd@asd.com>
2024-03-31 23:43:42 +0300

    Initial commit

---
+++ src/main/java/ru/inno/course/Iterator.java
@@ -0,0 +1,30 @@
+package ru.inno.course;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Iterator {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:postgresql://dpg-cn1542en7f5s73fdrigg-a.frankfurt-postgres.render.com/x_clients_xxet";
+
+        String user = "x_clients_user";
+        String password = "x7ngHjC1h08a85bELNifgKmqZa8KIR40";
+        String sql = "SELECT * FROM company";
+
+        Connection connection = DriverManager.getConnection(url, user, password);
+
+        ResultSet resultSet = connection.createStatement().executeQuery(sql);
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            System.out.println(id + " " + name);
+        }
+
+        connection.close();
+
+
+    }
+
+}
