7a89d4a2364614534ceb5428c4f7fae00e3d2e35
Roman Grigorev <roma@gmail.com>
2024-04-08 14:10:48 +0300

    яаыв

--- src/test/java/com/onrender/x_clients_be/web/x_clients/tests/EmployeeTests.java
+++ src/test/java/com/onrender/x_clients_be/web/x_clients/tests/EmployeeTests.java
@@ -7,6 +7,7 @@ import com.onrender.x_clients_be.web.x_clients.models.Employee;
 import com.onrender.x_clients_be.web.x_clients.models.UpdateEmployee;
 import com.onrender.x_clients_be.web.x_clients.setup.BaseTest;
 import com.onrender.x_clients_be.web.x_clients.utils.EmployeeUtils;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -24,14 +25,14 @@ public class EmployeeTests extends BaseTest {
     void testAddEmployee() {
         CreateEmployee employee = createEmployee();
         Integer employeeId = EmployeeUtils.addEmployee(employee, createCompany());
-        assertThat(employeeId, notNullValue());
+        assertNotNull(employeeId, "Failed to add employee");
     }
 
     @Test
     void testGetEmployee() {
         Integer employeeId = EmployeeUtils.addEmployee(createEmployee(), createCompany());
         Employee employeeInfo = EmployeeUtils.getEmployee(employeeId);
-        assertThat(employeeInfo, notNullValue());
+        assertNotNull(employeeInfo, "Failed to get employee");
     }
 
     @Test
@@ -40,7 +41,8 @@ public class EmployeeTests extends BaseTest {
         Integer employeeId = EmployeeUtils.addEmployee(employee, createCompany());
         UpdateEmployee updateEmployee = EmployeeGenerator.updateEmployee();
         Integer updatedEmployeeId = EmployeeUtils.updateEmployee(employeeId, updateEmployee);
-        assertNotEquals(employee.getEmail(),EmployeeUtils.getEmployee(updatedEmployeeId).getEmail());
+        assertNotEquals(employee.getEmail(), EmployeeUtils.getEmployee(updatedEmployeeId).getEmail(),
+                "Failed to update employee");
     }
 
     @Test
@@ -51,84 +53,13 @@ public class EmployeeTests extends BaseTest {
         Integer employeeIdThird = EmployeeUtils.addEmployee(createEmployee(), companyId);
         List<Employee> employeeList = EmployeeUtils.getEmployeeList(companyId);
         assertThat(employeeList, not(empty()));
+        assertNotNull(employeeList,  "Failed to retrieve employee list");
 
         assertThat(employeeList, containsInAnyOrder(
                 hasProperty("id", equalTo(employeeIdFirst)),
                 hasProperty("id", equalTo(employeeIdSecond)),
                 hasProperty("id", equalTo(employeeIdThird))
         ));
+
     }
-
-//    String token = TOKEN;
-//    static Faker faker = new Faker();
-//    EmployeeUtils employeeUtils = new EmployeeUtils();
-
-//    @Test
-//    void companyTest() {
-//        System.out.println("добавить новую компанию");
-//        int id = createCompany();
-//        System.out.println("Добавили новую компанию: " + " id = " +id);
-//        System.out.println();
-//
-//        Company company = getCompany(id);
-//        System.out.println("получить конкретную компанию");
-//        System.out.println(company);
-//        System.out.println();
-//
-//        System.out.println("проверим статус компании");
-//        System.out.println(company.isIsActive());
-//        System.out.println();
-//
-//        System.out.println("сделать компанию неактивной");
-//        System.out.println(updateCompanyStatus(id,false).isIsActive());
-//        System.out.println();
-//
-//        System.out.println("сделать компанию активной");
-//        System.out.println(updateCompanyStatus(id,true).isIsActive());
-//        System.out.println();
-//
-//        System.out.println("получим список всех компаний");
-//        List<Company> companies = getCompanies();
-//        System.out.println("Размер списка: " + companies.size());
-//        System.out.println();
-//
-//
-//        System.out.println("изменить компанию");
-//        System.out.println("старые названия: " + company.getName() + " " + company.getDescription());
-//        CreateCompany companyToAdd = CompanyGenerator.generateCompany();
-//        System.out.println("новые названия: " + companyToAdd.getName() + " " + companyToAdd.getDescription());
-//        Company company1 = changeCompany(id, companyToAdd);
-//        System.out.println("новые названия после изменения: " + company1.getName() + " " + company1.getDescription());
-//        System.out.println();
-//
-//        System.out.println("удалить компанию");
-//        Company company2 = deleteCompany(id);
-//        System.out.println("удалили компанию: " + company2.getName() + " " + company2.getDescription());
-//        Company company3 = getCompany(id);
-//        assertNull(company3);
-//        System.out.println("компание удалена");
-//
-//    }
-//
-//    @Test
-//    void employeeTest() {
-//        System.out.println("Создать нового сотрудника");
-//        int id = addEmployee(createEmployee(),createCompany());
-//        System.out.println();
-//
-//        System.out.println("Получить сотрудника");
-//        System.out.println(getEmployee(id));
-//        System.out.println();
-//
-//
-//        System.out.println("Изменить сотрудника");
-//        System.out.println(updateEmployee(id, EmployeeGenerator.updateEmployee()));
-//        System.out.println(getEmployee(id));
-//        System.out.println();
-//
-//        System.out.println("Получить список сотрудников");
-//        System.out.println(getEmployeeList(3527));
-//
-//
-//    }
 }
